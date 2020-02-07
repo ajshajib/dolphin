e53a4eb7b830b0ca793056a8af069a85e39da6b9
Anowar Jaman Shajib <ajshajib@gmail.com>
2020-02-06 17:11:03 -0800

    Add modules and storage structure

---
+++ dolphin/initializer/load_data.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Loads reduced data and prepares for modelliing.
+"""
+
+
+class DataLoader(object):
+    """
+
+    """
+
+    def __init__(self):
+        """
+
+        """
+        pass

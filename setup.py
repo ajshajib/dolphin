0a14e7505b6f9907d89b87f8c84b75bc2ed31964
Anowar Jaman Shajib <ajshajib@gmail.com>
2020-03-11 16:37:53 -0700

    Remove Python 2.7 support, add Python 3.8 testing

--- setup.py
+++ setup.py
@@ -46,10 +46,11 @@ setup(
         'Natural Language :: English',
         # 'Programming Language :: Python :: 2',
         # 'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         #'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )

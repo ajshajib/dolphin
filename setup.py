6a9543e4d68795ff04a25f382a61e830d0b42c82
Anowar Shajib <ajshajib@uchicago.edu>
2024-09-04 22:20:13 -0500

    Refactor setup.py to update version from 0.0.1 to 0.0.2

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/ajshajib/dolphin",
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
 )

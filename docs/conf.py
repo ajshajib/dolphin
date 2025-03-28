c4167cfb00bf7659612995cb1633ca19cb93bde7
Anowar Shajib <ajshajib@gmail.com>
2025-03-28 16:14:17 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,13 @@ import dolphin
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    # other extensions
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -100,12 +106,20 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "PyData"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "logo": {
+        "text": "dolphin",
+    },
+    "navbar_end": ["search-field", "navbar-icon-links"],
+    "external_links": [
+        {"name": "GitHub", "url": "https://github.com/ajshajib/dolphin"},
+    ],
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []

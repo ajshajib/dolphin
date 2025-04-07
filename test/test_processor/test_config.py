566377c9212c4bb5813645b0c10b5dd823cac234
Anowar Shajib <ajshajib@uchicago.edu>
2025-04-07 18:39:45 -0500

    Add additional test case for joint_lens_with_light configuration

--- test/test_processor/test_config.py
+++ test/test_processor/test_config.py
@@ -215,6 +215,7 @@ class TestModelConfig(object):
             "joint_source_with_point_source": [],
             "joint_lens_with_light": [
                 [0, 0, ["center_x", "center_y"]],
+                [4, 2, ["center_x", "center_y"]],
                 [5, 3, ["center_x", "center_y"]],
                 [6, 2, ["center_x", "center_y"]],
                 [7, 3, ["center_x", "center_y"]],

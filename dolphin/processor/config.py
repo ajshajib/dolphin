d2a96163e504050ebc592dabbcffb7c0420dacf9
Anowar Shajib <ajshajib@uchicago.edu>
2026-03-27 13:06:04 -0500

    Add parameters for lens model bounds in get_lens_model_params method

--- dolphin/processor/config.py
+++ dolphin/processor/config.py
@@ -886,9 +886,13 @@ class ModelConfig(Config):
         multiple filters)"""
         return self.get_index_list("source_light")
 
-    def get_lens_model_params(self):
+    def get_lens_model_params(self, theta_E_upper_factor=1.5, theta_E_lower_factor=0.3):
         """Create `lens_params`.
 
+        :param theta_E_upper_factor: Factor to multiply the initial Einstein radius for the upper bound
+        :type theta_E_upper_factor: `float`
+        :param theta_E_lower_factor: Factor to multiply the initial Einstein radius for the lower bound
+        :type theta_E_lower_factor: `float`
         :return:
         :rtype:
         """
@@ -949,7 +953,7 @@ class ModelConfig(Config):
 
                 lower.append(
                     {
-                        "theta_E": 0.3,
+                        "theta_E": theta_E_lower_factor * theta_E_init,
                         "e1": -0.5,
                         "e2": -0.5,
                         "gamma": 1.3,
@@ -960,7 +964,7 @@ class ModelConfig(Config):
 
                 upper.append(
                     {
-                        "theta_E": 3.0,
+                        "theta_E": theta_E_upper_factor * theta_E_init,
                         "e1": 0.5,
                         "e2": 0.5,
                         "gamma": 2.8,
@@ -992,14 +996,14 @@ class ModelConfig(Config):
                 )
                 lower.append(
                     {
-                        "theta_E": 0.0,
+                        "theta_E": theta_E_lower_factor * theta_E_init,
                         "center_x": center_x - bound,
                         "center_y": center_y - bound,
                     }
                 )
                 upper.append(
                     {
-                        "theta_E": 1.0,
+                        "theta_E": theta_E_upper_factor * theta_E_init,
                         "center_x": center_x + bound,
                         "center_y": center_y + bound,
                     }

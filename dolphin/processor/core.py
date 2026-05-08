c1e34f2f52e1778fe8b09e7680d3a885f479935f
Ryan Brady <ryan.m.brady@stonybrook.edu>
2026-05-08 13:50:59 -0400

    Add time_delay_likelihood support

--- dolphin/processor/core.py
+++ dolphin/processor/core.py
@@ -9,6 +9,7 @@ from .. import __version__
 
 from lenstronomy.Workflow.fitting_sequence import FittingSequence
 from schwimmbad import choose_pool
+import numpy as np
 
 from .files import FileSystem
 from .config import ModelConfig
@@ -175,6 +176,16 @@ class Processor(object):
             "multi_band_list": multi_band_list,
             "multi_band_type": "multi-linear",
         }
+
+        if "time_delays_measured" in config.settings["point_source_option"]:
+            kwargs_data_joint.update({
+                "time_delays_measured": np.array(
+                    config.settings["point_source_option"]["time_delays_measured"]
+                ),
+                "time_delays_uncertainties": np.array(
+                    config.settings["point_source_option"]["time_delays_uncertainties"]
+                )
+            })
 
         return kwargs_data_joint
 
--- test/test_processor/test_core.py
+++ test/test_processor/test_core.py
@@ -4,6 +4,8 @@
 from pathlib import Path
 
 from dolphin.processor.core import Processor
+import numpy as np
+import numpy.testing as npt
 
 _ROOT_DIR = Path(__file__).resolve().parents[2]
 _TEST_IO_DIR = _ROOT_DIR / "io_directory_example"
@@ -29,14 +31,27 @@ class TestProcessor(object):
         )
 
     def test_get_kwargs_data_joint(self):
-        """Test `get_kwargs_data_joint` method."""
+        """Test `get_kwargs_data_joint` method."""        
         kwargs_data_joint = self.processor.get_kwargs_data_joint("lens_system1")
 
         assert kwargs_data_joint["multi_band_type"] == "multi-linear"
 
         assert len(kwargs_data_joint["multi_band_list"]) == 1
-        assert len(kwargs_data_joint["multi_band_list"][0]) == 3
+        assert len(kwargs_data_joint["multi_band_list"][0]) == 3        
 
+        kwargs_data_joint = self.processor.get_kwargs_data_joint("lens_system5")
+
+        npt.assert_array_equal(
+            kwargs_data_joint["time_delays_measured"],
+            [1., 1., 1.]
+        )
+        npt.assert_array_equal(
+            kwargs_data_joint["time_delays_uncertainties"],
+            [[0.5, 0., 0.],
+            [0., 0.5, 0.],
+            [0., 0., 0.5]]
+        )
+    
     def test_get_image_data(self):
         """Test `get_image_data` method."""
         image_data = self.processor.get_image_data("lens_system1", "F390W")
